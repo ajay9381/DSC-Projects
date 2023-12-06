c67a3507fcdd6e5e4240edb498c22e917b6eb76d
YOUR NAME <apadala@DESKTOP-0P9M4GB.localdomain>
2023-12-06 02:52:06 -0500

    lab9 answers

---
+++ lab9/HelloMPI.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <mpi.h>//include mpi library header
+int main(int argc,char **argv)
+{
+int rank,size;
+MPI_Init(&argc,&argv);//initializes MPI
+MPI_Comm_rank(MPI_COMM_WORLD,&rank); //get current MPIâˆ’process ID . O, 1 , . . .
+MPI_Comm_size(MPI_COMM_WORLD, &size); //get the total number of processes
+printf("Hello world from process %d of %d\n",rank,size);
+MPI_Finalize();//programs should always perform a "graceful" shutdown
+return 0;
+}
+
